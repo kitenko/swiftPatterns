0ee72dd023a6ebf2d1b35d3e701ba086f7468b00
Andrey Kitenko <kitenko.andrey@gmail.com>
2022-08-15 10:47:50 +0300

    added MVC

---
+++ RabbleWabble_MVC/RabbleWabble/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Andrey KItenko on 14.08.2022.
+//
+
+import Foundation
+
+public struct Question {
+  public let answer: String
+  public let hint: String?
+  public let prompt: String
+}
---
+++ RabbleWabble_MVC/RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by Andrey KItenko on 14.08.2022.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+  public let questions: [Question]
+  public let title: String
+}
